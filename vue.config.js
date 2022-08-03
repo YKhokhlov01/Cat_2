4acfc6ec6a7bdbd7314284d52d55feca324e5507
YKhokhlov01 <ykh00011@gmail.com>
2022-08-03 11:57:32 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  module: {
-    rules: [
-      // ... другие правила опущены
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              // включаем CSS модули
-              modules: true,
-              // настраиваем генерируемое имя класса
-              localIdentName: '[local]_[hash:base64:8]'
-            }
-          }
-        ]
-      }
-    ]
-  },
+  
   transpileDependencies: true
 })
