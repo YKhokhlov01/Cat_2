cedd6c6c73ac4c72766d2e80fdd0df10046f30fd
YKhokhlov01 <ykh00011@gmail.com>
2022-08-04 13:30:39 +0300

    6.3
    6.3

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
   ?'/cat_1/'
-  : '/',
+  : '/'
 
-  transpileDependencies: true
+ 
 })
